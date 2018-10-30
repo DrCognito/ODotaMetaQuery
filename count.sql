f8d7161d59c4b178a2564c5533c13f29fd6c34ad
M Chadwick <dr.m.chadwick@gmail.com>
2018-10-30 15:59:02 +0000

    Count update for amateur matches.

--- count.sql
+++ count.sql
@@ -1,8 +1,9 @@
-SELECT
+SELECT 
 COUNT (*)
-FROM
-public_matches
+FROM 
+public_matches 
 WHERE public_matches.avg_mmr >= 5000
 AND public_matches.avg_rank_tier >= 72
-AND public_matches.start_time > REP_TIME
+AND public_matches.start_time > 1540304204
 AND public_matches.avg_rank_tier IS NOT NULL
+AND public_matches.avg_mmr IS NOT NULL
